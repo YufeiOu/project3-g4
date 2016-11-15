cd10840f95398aa2f6928473e6c59d1a8d72837f
Yufei Ou <yo2265@columbia.edu>
2016-11-15 18:50:23 -0500

    new architecture skeleton code

--- Player.java
+++ Player.java
@@ -17,42 +17,39 @@ public class Player implements sqdance.sim.Player {
 	private int boredTime = 120; // 2 minutes
 
 	private int d = -1;
-	private double room_side = -1;
+	private int room_side = -1;
 
 	private int[] soulmate; // initialize to -1
 	private int[][] relation; // kind of relation: 1 for soulmate, 2 for friend, 3 for stranger, initialize to -1
 
 	private int[][] danced; // cumulatived time in seconds for dance together
 
-	//======= data structures for snake strategy =========
-	//pit positions; don't change after initialization
-	private Point[] positions;
-	private Point[] partnerPos;
-
-
-	//copy of positions returned to the simulator
-	private Point[] copy;
-	private boolean start;
-
-	//player ids occupying each pit
-	private int[] player_ids;
-	//pit ids occupied by each player
-	private int[] pit_ids;
-
 	private int stayed;
 
-	private double delta = 1e-3;
-	private double danceDis = minDis + delta;
-	private double keepDis = danceDis + delta;
+	public class Dancer{
+		int id = -1;
+		int soulmate = -1;
+		int des_pit = -1;
+		Point next_pos = null;
+	}
 
-	
-	//private MetaData[] data;
+	//dancers never stay at pit, legal positions are up/down/left/right eps/3;
+	public class Pit{
+		Point pos = null;
+		Point prev = null;
+		Point next = null;
+	}
+
+	private Dancer[] dancers;
+	private boolean connected;
+	private Point[] starting_positions;
+	private Pit[] pits;
 	//====================== end =========================
 
 	public void init(int d, int room_side) {
 		//System.out.println("init");
 		this.d = d;
-		this.room_side = (double) room_side;
+		this.room_side = room_side;
 		
 		//data structure initialization
 		soulmate = new int[d];
@@ -67,81 +64,73 @@ public class Player implements sqdance.sim.Player {
 			}
 		}
 
-		this.stayed =  0;
-
-		//initialization for snake strategy
-		ArrayList<ArrayList<Point>> pits = new ArrayList<ArrayList<Point>>();
-		int numSlot = 0, numPit = 0;
-		for (double x = eps; x < room_side && numPit < d; x += danceDis + keepDis) {
-			pits.add(new ArrayList<Point>());
-			if ((numSlot&1) == 0) {
-				for (double y = eps; y < room_side && numPit < d; y += keepDis) {
-					pits.get(numSlot).add(new Point(x, y));
-					numPit += 2;
-				}
-			} else {
-				for (double y = room_side - eps; y > 0 && numPit < d; y -= keepDis) {
-					pits.get(numSlot).add(new Point(x, y));
-					numPit += 2;
-				}
+		this.connected = true;
+		this.starting_positions = new Point[d];
+		this.pits = new Pit[d];
+		boolean odd = true;
+		Point prev = null;
+		Point curr = null;
+		double x = eps;
+		double y = eps;
+		double increment = 0.5 + eps;
+		while(i<d){
+			int sign = (((i&1) == 1) ? 1: -1);
+			if(y + sign * increment <= this.room_side - eps && y + sign * increment >= eps){
+				y += sign * increment;
 			}
-			++numSlot;
+			else{
+				x += increment;
+			}
+			curr = new Point(x,y);
+			this.starting_positions[i] = curr;
+			this.pits[i] = new Pit();
+			this.pits[i].prev = prev;
+			this.pits[i].curr = curr;
+			prev = curr;
+			Dancer dancer = new Dancer();
+			dancer.id = i;
+			dancer.des_pit = i;
+			this.dancers[i] = dancer;
 		}
+	}
+}
 
-		this.positions = new Point[d];
-		this.player_ids = new int[d];
-		this.partnerPos = new Point[d];
-		this.pit_ids= new int[d];
-		for(int i = 0; i < d; i++){
-			player_ids[i] = i;
-			pit_ids[i] = i;
+public Point[] generate_starting_locations() {
+	return this.starting_positions;
+}
+
+	public Point[] play(Point[] old_positions, int[] scores, int[] partner_ids, int[] enjoyment_gained) {
+		// first update partner information and culmulative time danced
+		boolean new_soulmate_found = updatePartnerInfo(partner_ids, enjoyment_gained);
+		if (connected && !new_soulmate_found) {
+			swap();
+		}
+		else{
+			// generate a sequence of pit indexes of de-coupled dancers
+			genShape();
+			//try to connect the dancers
+			connect();
 		} 
-		int cur = 0;
-		for (int i = 0; i < numSlot; ++i) {
-			for (int j = 0; j < pits.get(i).size(); ++j) {
-				Point p = pits.get(i).get(j);
-				if ((i&1) == 1){
-					this.partnerPos[cur] = new Point(p.x,p.y);
-					this.positions[cur++] = new Point(p.x + danceDis, p.y);
-				}
-				else{
-					this.partnerPos[cur] = new Point(p.x + danceDis, p.y);
-					this.positions[cur++] = new Point(p.x,p.y);
-				}
-			}
-		}
-		for (int i = numSlot - 1; i >= 0; --i) {
-			for (int j = pits.get(i).size() - 1; j >= 0; --j) {
-				Point p = pits.get(i).get(j);
-				if ((i&1) == 0){
-					this.partnerPos[cur] = new Point(p.x,p.y);
-					this.positions[cur++] = new Point(p.x + danceDis, p.y);
-				}
-				else{
-					this.partnerPos[cur] = new Point(p.x + danceDis, p.y);
-					this.positions[cur++] = new Point(p.x,p.y);
-				} 
-			}
-		}	
-
-		this.copy = new Point[d];
-		for(int i = 0; i < d; i++) this.copy[i] = new Point(this.positions[i].x,this.positions[i].y);
+		move_couple();
+		//generate instructions using target positions and current positions
+		return generateInstructions(current_positions);
 	}
 
-	public Point[] generate_starting_locations() {
-		return this.copy;
+	//modify the desination positions of active dancers;
+	void swap(){
+
 	}
 
-	public Point[] play(Point[] dancers, int[] scores, int[] partner_ids, int[] enjoyment_gained) {
-		Point[] stay = new Point[d];
-		for (int i = 0; i < d; ++i) {
-			stay[i] = new Point(0., 0.);
-		}
 
-		//first update partner information and culmulative time danced
+
+
+
+	boolean updatePartnerInfo(int[] partner_ids, int[] enjoyment_gained) {
+		boolean found = false;
 		for(int i = 0; i < d; i++){
 			if(enjoyment_gained[i] == 6){
 				soulmate[i] = partner_ids[i];
+				if(relation[i][partner_ids[i]] != 1) found = true;
 				relation[i][partner_ids[i]] = 1;
 			}
 			else if(enjoyment_gained[i] == 4){
@@ -152,77 +141,19 @@ public class Player implements sqdance.sim.Player {
 			}
 			danced[i][partner_ids[i]] += 6;
 		}
-
-		if(stayed < boredTime){
-			stayed += 6;
-			return stay;
-		}
-		stayed = 0;
-		
-
-		Point[] new_Positions = new Point[d];
-		int[] new_pit_ids = new int[d];
-		int[] new_player_ids = new int[d];
-		//move snake by one rotation
-		for (int i = 0; i < d; ++i) {
-			new_Positions[i] = new Point(positions[(pit_ids[i]+1)%d].x,positions[(pit_ids[i]+1)%d].y);
-			new_pit_ids[i] = (pit_ids[i] + 1)%d;
-			new_player_ids[(pit_ids[i]+1)%d] = i;
-		}
-
-		boolean[] swaped = new boolean[d];
-		//won't have enjoyment next round, swap dancer across slot
-		for(int j = 0; j < d; j++){
-			if(!swaped[j]){
-				for(int k = 0; k < d; k++){
-					Point partner = new_Positions[k];
-					if(k == j || relation[j][k] != 3 ||  danced[j][k] < boredTime || !samepos(partner,partnerPos[new_pit_ids[j]])) continue;
-					swaped[j] = true;
-					swaped[k] = true;
-					System.out.println("swaped player: " + j + "," + k);
-					Point p1 = new_Positions[j];
-					Point p2 = partner;
-					new_Positions[j] = p2;
-					new_Positions[k] = p1;
-					int pit_id1 = new_pit_ids[j];
-					int pit_id2 = new_pit_ids[k];
-					new_pit_ids[j] = pit_id2;
-					new_pit_ids[k] = pit_id1;
-					new_player_ids[pit_id1] = k;
-					new_player_ids[pit_id2] = j;
-					break;
-				}
-			}
-		}
-
-		pit_ids = new_pit_ids;
-		player_ids = new_player_ids;
-		return generateInstructions(dancers,new_Positions);
+		return found;
 	}
 
 
-	private Point[] generateInstructions(Point[] oldPositions,Point[] new_Positions){
+	private Point[] generateInstructions(Point[] currentPositions){
 		Point[] res = new Point[d];
-		for(int i = 0; i < d; i++){
-			res[i] = new Point(new_Positions[i].x-oldPositions[i].x,new_Positions[i].y-oldPositions[i].y);
+		for(Dancer dancer:dancers){
+			res[i] = new Point(dancer.next_pos.x-currentPositions[i].x,dancer.next_pos.y-currentPositions[i].y);
 		}
 		return res;
 	}
 
 	private boolean samepos(Point p1,Point p2){
-		return p1.x == p2.x && p1.y == p2.y;
-	}
-
-	private void printSnakePositions() {
-		boolean print = false;
-		for(int i = 0; i < d; i++){
-			if(copy[i].x != positions[i].x || copy[i].y != positions[i].y) print = true;
-		}
-		
-		if(print){
-			for (int i = 0; i < d; ++i) {
-				System.out.format("%.1f,%.1f\n",positions[i].x,positions[i].y);
-			}
-		}
+		return Math.abs(p1.x - p2.x) < eps && Math.abs(p1.y - p2.y) < eps;
 	}
 }
